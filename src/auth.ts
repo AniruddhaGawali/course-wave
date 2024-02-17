9a5606e8de6792767f8bef71329968d1e755b2ba
Aniruddha Gawali <aniruddhagawali03@gmail.com>
2024-02-17 21:29:41 +0530

    Update auth.ts file with new imports and configurations

--- src/auth.ts
+++ src/auth.ts
@@ -1,18 +1,19 @@
-import NextAuth, { AuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import db from '@/db';
-import { Adapter } from 'next-auth/adapters';
+import NextAuth, { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import db from "@/db";
+import { Adapter } from "next-auth/adapters";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
 
+  secret: process.env.AUTH_SECRET,
   callbacks: {
     async session({ session, user }) {
       session.user.id = user.id;
